06833ff9dfbc7ce3c34768d139338bbc3b5dd027
Sarangverma98 <sarangverma98@gmail.com>
2023-05-16 14:55:54 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://sarangverma98.github.io/movieMania',
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
 })
